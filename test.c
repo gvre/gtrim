0bf445597bd8045175ca82b2594299edece93e59
Giannis Vrentzos <gvre@gvre.gr>
2015-12-31 19:36:24 +0200

    Removed length check in test.c

--- test.c
+++ test.c
@@ -11,24 +11,24 @@ int main(void)
     char *msg = malloc(20);
     strcpy(msg, "		       hello ");
     msg = trim(msg);
-    assert(strcmp(msg, "hello") == 0 && strlen(msg) == 5);
+    assert(strcmp(msg, "hello") == 0);
     free(msg);
 
     char msg2[] = "  world  ";
     trim(msg2);
-    assert(strcmp(msg2, "world") == 0 && strlen(msg2) == 5); 
+    assert(strcmp(msg2, "world") == 0);
 
     char msg3[] = "  hello world  ";
     trim(msg3);
-    assert(strcmp(msg3, "hello world") == 0 && strlen(msg3) == 11); 
+    assert(strcmp(msg3, "hello world") == 0);
 
     char msg4[] = "  world  ";
     ltrim(msg4);
-    assert(strcmp(msg4, "world  ") == 0 && strlen(msg4) == 7); 
+    assert(strcmp(msg4, "world  ") == 0);
 
     char msg5[] = "  world  ";
     rtrim(msg5);
-    assert(strcmp(msg5, "  world") == 0 && strlen(msg5) == 7); 
+    assert(strcmp(msg5, "  world") == 0);
 
     char *msg6 = "";
     msg6 = trim(msg6);
